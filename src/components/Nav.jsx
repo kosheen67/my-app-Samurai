b85a75cb729044cfc7ddc763e177b6e205ade987
kosheen67 <kosheen67@gmail.com>
2026-01-03 21:25:40 +0300

    +

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,20 @@
+import classes from './Nav.module.css'
+
 const Nav = () => {
-    return (<nav className='nav'>
-        <div>
+    return (<nav className={classes.nav}>
+        <div className={classes.item}>
             <a>Profile</a>
         </div>
-        <div>
+        <div className={classes.item}>
             <a>Messages</a>
         </div>
-        <div>
+        <div className={classes.item}>
             <a>News</a>
         </div>
-        <div>
+        <div className={classes.item}>
             <a>Music</a>
         </div>
-        <div>
+        <div className={classes.item}>
             <a>Settings</a>
         </div>
     </nav>)
